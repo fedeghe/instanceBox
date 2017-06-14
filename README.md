0d42ce279d86be8bf222fb368a134c6e6c190db3
Federico Ghedina <fedeghe@gmail.com>
2017-06-15 01:07:37 +0200

    first browser test, soon will be done with chai and sinon

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# instanceBox
-use localstorage to store instances
+# instanceBox  
+**What**: You want to store javascript instances in the browser  
+**Why**: I dont care 
+**How**: this module aims to offer a solution
+
+### build and run a pseudo test  
+- npm install
+- ./node_modules/malta/src/bin.js build.json (let it run)
+- open index.html 
--- source/lib/instanceBox.js
+++ source/lib/instanceBox.js
@@ -1,109 +1,65 @@
 FG = {};
 FG.instanceBox = (function (){
 	"use strict";
-	var tools = {
-		encoder : {
-			encode : function (el) {
-				switch(typeof el) {
-					case "string": return {nature : "string", val : el};
-					case "number": return {nature : "number", val : el};
-					case "boolean": return {nature : "boolean", val : el};
-					case 'function': return {nature : "function", val : el.toString()};
-					case 'object': return {nature : 'object', val : JSON.stringify(el)};
-					case null : return null;
-					default : return null;
-				}
-			},
-			decode : function (el) {
-
-				switch(el.nature) {
-					case "string":	
-					case "number":
-					case "boolean": return el.val;
-					case 'function': return eval("(" + el.val + ")");
-					case 'object': return JSON.parse(el.val);
-					case null : 
-						return null;
-				}
-			}
-		},
-		base64 : {
+	var storage = localStorage,
+		base64 = {
 			forth : function (str) {
-    			return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
-    				function toSolidBytes(match, p1) {
-    					return String.fromCharCode('0x' + p1);
-    				}
-    			));
-    		},
-    		back : function (str) {
+				return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+					function toSolidBytes(match, p1) {
+						return String.fromCharCode('0x' + p1);
+					}
+				));
+			},
+			back : function (str) {
 				return decodeURIComponent(atob(str).split('').map(function(c) {
 					return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
 				}).join(''));
 			}
-		}
-	};
-	function get(fr){
-		var f = eval("(" + fr.constructor + ")"),
-			o = new f(), i;
-		o.prototype = {};
-		for (i in fr.props) {
-			o[i] = tools.encoder.decode(fr.props[i]);
-		}
-		for (i in fr.proto){
-			f.prototype[i] = eval("(" + fr.proto[i] + ")");
-		}
-		return o;
-	}
-
-	function set (o){
-		var constructorProto = o.constructor.prototype,
-			props = {},
-			proto = {};
-		try {
-			// owned
-			for (var i in o) {
-				if (o.hasOwnProperty(i)){
-					props[i] = tools.encoder.encode(o[i]);
-				}
-			}
-			// proto
-			for (var i in constructorProto) {
-				proto[i] = constructorProto[i].toString();
-			}
-		} catch(e){
-			return false;
-		}
-		return {
-			constructor : o.constructor.toString(),
-			constructorName : o.constructor.name,
-			props : props,
-			proto : proto
-		};
-	} 
-	var base64 = false,
+		},
 		store = {
 			base64 : false,
 			setItem : function (k, el) {
-				var w = JSON.stringify(set(el));
-				if (this.base64) {
-					w = tools.base64.forth(w);
+				var v = JSON.stringify({
+					constructorName : el.constructor.name,
+					el : el
+				})
+				v = this.base64 ? base64.forth(v) : v;
+				storage.setItem(getKey(k), v);
+			},
+			getItem : function (k) {
+				var cnt = storage.getItem(getKey(k));
+				if (cnt == null) return null;
+				var	o = JSON.parse(this.base64 ? base64.back(cnt) : cnt),
+					constructorName = o.constructorName,
+					el = o.el,
+					fn = eval(constructorName),
+					res = new fn(),
+					i;
+				for (i in el)
+					if (el.hasOwnProperty(i))
+						res[i] = el[i];
+				return res;
+			},
+			key : function (n) {
+				var k = storage.key(n);
+				k = k.replace(/$iB-/, ''); 
+				return k;
+			},
+			removeItem : function (k) {storage.removeItem(getKey(k));},
+			clear : function () {
+				storage.clear();
+			},
+			length : function () {return storage.length;},
+			use : function (st) {
+				switch (st) {
+					case 'local': storage = localStorage; break;
+					case 'session': storage = sessionStorage; break;
 				}
-				localStorage.setItem(k, w);
-			},
-			getItem : function (k, cls) {
-				var w = localStorage.getItem(k),
-					obj;
-				if (this.base64) w = tools.base64.back(w);
-				obj = get(JSON.parse(w));
-				Object.setPrototypeOf(obj, cls.prototype);
-				return obj;
-			},
-			key : function (n) {return localStorage.key(n);},
-			removeItem : function (k) {localStorage.removeItem(k); },
-			clear : function () {localStorage.clear(); },
-			length : function () {return localStorage.length;}
+			}
 		};
+	function getKey(k) {return "iB-" + k;}
 	return {
+		use : store.use,
 		base64 : store.base64,
 		set : store.setItem,
 		get : store.getItem,
@@ -113,3 +69,10 @@ FG.instanceBox = (function (){
 		key : store.key
 	};
 }());
+
+
+
+ 
+
+
+
